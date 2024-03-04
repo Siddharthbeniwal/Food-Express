1d4f9995fc36aaa841138db85bca6c5bd51eda38
Siddharth Beniwal <siddharthben16@gmail.com>
2024-03-05 01:08:07 +0530

    Added info and default user

--- src/appConstants.js
+++ src/appConstants.js
@@ -6,8 +6,17 @@ export const API_URLS = {
   GET_MY_ORDERS: 'http://localhost:5000/api/getMyOrders'
 };
 
-export const ABOUT_US_TEXT = 
-`"Food Express" is a food ordering app developed by Siddharth Beniwal using MERN stack (MongoDB, Express.js, React.js, Node.js) 
-It is seamlessly integrated with Redux Toolkit for state management. It uses JWT for authentication tokens which enhances the security 
-of the system by providing a secure and efficient way to verify user identity. Additionally, it uses bcrypt to hash and store encrypted
-passwords which adds an extra layer of protection, ensuring that sensitive user data remains confidential.`
+export const ABOUT_US_INFO = {
+
+  ABOUT_US_TEXT:
+    `'Food Express' is a food ordering app developed by Siddharth Beniwal using MERN stack (MongoDB, Express.js, React.js, Node.js) 
+    It is seamlessly integrated with Redux Toolkit for state management. It uses JWT for authentication tokens which enhances the security 
+    of the system by providing a secure and efficient way to verify user identity. Additionally, it uses bcrypt to hash and store encrypted
+    passwords which adds an extra layer of protection, ensuring that sensitive user data remains confidential.`,
+
+  LOGIN_INFO_1: 'Go to top-right corner and Sign Up using email id and password and then Login with the same credentials.',
+
+  LOGIN_INFO_2: 'Directly Login with dummy user:',
+
+  CREDENTIAL: 'johndoe@gmail.com'
+}
